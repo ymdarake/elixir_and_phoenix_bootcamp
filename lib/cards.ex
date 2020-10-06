5264d34d250bffd21b753f584ffabadd89ae699a
ymdarake <t.sakamoto.ouen@gmail.com>
2020-10-06 17:55:06 +1300

    [Sec1]Enum

--- lib/cards.ex
+++ lib/cards.ex
@@ -12,7 +12,24 @@ defmodule Cards do
       :world
 
   """
-  def hello do
-    :world
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades",  "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 end
