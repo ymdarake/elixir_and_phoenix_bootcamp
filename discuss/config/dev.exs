12cc31448a20e4cee7cc5c53e15966393ad2b6f1
ymdarake <t.sakamoto.ouen@gmail.com>
2020-10-07 11:02:17 +1300

    [55]Config PostgreSQL, hot-reload

--- discuss/config/dev.exs
+++ discuss/config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :discuss, Discuss.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "d150022",
+  password: "",
   database: "discuss_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
