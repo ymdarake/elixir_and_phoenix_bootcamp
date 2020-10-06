966bc978c585f0535c5423eee35b758fbd3d731f
ymdarake <t.sakamoto.ouen@gmail.com>
2020-10-06 22:22:55 +1300

    [43, 44]Mirroring a Row, Mapping with Custom Functions

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,12 +3,21 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
-    |>build_grid
+    |> build_grid
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
     hex
     |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row(row) do
+    # [145, 46, 200]
+    [first, second | _tail] = row
+
+    # [145, 46, 200, 46, 145]
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
