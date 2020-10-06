e3ec34c41e6f40892433a406d462096e8222ef4c
ymdarake <t.sakamoto.ouen@gmail.com>
2020-10-07 12:55:21 +1300

    [74]New Topic Form

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,10 +1,10 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
+	alias Discuss.Topic
+
 	def new(conn, params) do
-		IO.puts "++++"
-		IO.inspect conn
-		IO.puts "++++"
-		IO.inspect params
-		IO.puts "++++"
+		changeset = Topic.changeset(%Topic{}, %{})
+
+		render conn, "new.html"
 	end
 end
