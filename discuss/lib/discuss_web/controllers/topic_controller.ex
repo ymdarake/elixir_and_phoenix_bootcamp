903aae8cc1c2c833fd9cf4a7731af5a6f9b855f3
ymdarake <t.sakamoto.ouen@gmail.com>
2020-10-07 14:58:26 +1300

    [78]Introduction to  the Repo Module

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,6 +1,7 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 	alias Discuss.Topic
+	alias Discuss.Repo
 
 	def new(conn, _params) do
 		changeset = Topic.changeset(%Topic{}, %{})
@@ -9,5 +10,11 @@ defmodule DiscussWeb.TopicController do
 	end
 
 	def create(conn, %{"topic" => topic}) do
+		changeset = Topic.changeset(%Topic{}, topic)
+
+		case Repo.insert(changeset) do
+			{:ok, post} -> IO.inspect(post)
+			{:error, changeset} ->IO.inspect(changeset)
+		end
 	end
 end
