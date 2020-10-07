2b44f6ce0eaa54775e5c3c74e5f2e6eba2a7847e
ymdarake <t.sakamoto.ouen@gmail.com>
2020-10-07 16:45:01 +1300

    [97]Defining OAuth Routes

---
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -0,0 +1,12 @@
+defmodule Discuss.AuthController do
+  use DiscussWeb, :controller
+	plug Ueberauth
+
+	def callback(conn, params) do
+		IO.puts "+++++"
+		IO.inspect(conn.assigns)
+		IO.puts "+++++"
+		IO.inspect(params)
+		IO.puts "+++++"
+	end
+end
