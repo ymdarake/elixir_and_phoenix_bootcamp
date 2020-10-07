2c7f08931e792c4174469b8b60ee91e3fc2c9a02
ymdarake <t.sakamoto.ouen@gmail.com>
2020-10-07 16:31:39 +1300

    [96]Creating a Github OAuth App

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -29,3 +29,12 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.Oauth,
+  client_id: "FIXME",
+  client_secret: "FIXME"
