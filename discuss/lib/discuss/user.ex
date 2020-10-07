a3c4198fd2d72571adf4d6f642047dad8609be85
ymdarake <t.sakamoto.ouen@gmail.com>
2020-10-07 17:21:26 +1300

    [100 - 104]Handling OAuth Success

---
+++ discuss/lib/discuss/user.ex
@@ -0,0 +1,22 @@
+defmodule Discuss.User do
+  use Ecto.Repo,
+      otp_app: :discuss,
+      adapter: Ecto.Adapters.Postgres
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "users" do
+    field :email, :string
+    field :provider, :string
+    field :token, :string
+
+    timestamps()
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:email, :provider, :token])
+    |> validate_required([:email, :provider, :token])
+  end
+end
--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -1,8 +1,37 @@
 defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
+	alias Discuss.User
+	alias Discuss.Repo
 	plug Ueberauth
 
 	def callback(conn, params) do
+		%{assigns: %{ueberauth_auth: auth}} = conn
+		user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+		changeset = User.changeset(%User{}, user_params)
 
+		signin(conn, changeset)
+	end
+
+	defp signin(conn, changeset) do
+		case insert_or_update_user(changeset) do
+			{:ok, user} ->
+				conn
+				|> put_flash(:info, "Welcom back!")
+				|> put_session(:user_id, user.id)
+				|> redirect(to: Routes.topic_path(conn, :index))
+			{:error, _reason} ->
+				conn
+				|> put_flash(:error, "Error signing in")
+				|> redirect(to: Routes.topic_path(conn, :index))
+		end
+	end
+
+	defp insert_or_update_user(changeset) do
+		case Repo.get_by(User, email: changeset.changes.email) do
+			nil ->
+				Repo.insert(changeset)
+			user ->
+				{:ok, user}
+		end
 	end
 end
