7ada60cf2d9f33a0574f91b2c4bcdc863b90c121
ymdarake <t.sakamoto.ouen@gmail.com>
2020-10-07 15:15:18 +1300

    [81 - 83]Building List

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -3,6 +3,11 @@ defmodule DiscussWeb.TopicController do
 	alias Discuss.Topic
 	alias Discuss.Repo
 
+	def index(conn, _params) do
+		topics = Repo.all(Topic)
+		render conn, "index.html", topics: topics
+	end
+
 	def new(conn, _params) do
 		changeset = Topic.changeset(%Topic{}, %{})
 
