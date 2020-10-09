7e2d18b89b7ed6f2b9465ad93f9022cacc3d26cd
ymdarake <t.sakamoto.ouen@gmail.com>
2020-10-10 12:11:03 +1300

    [121]The Comments Model

--- discuss/lib/discuss/comment.ex
+++ discuss/lib/discuss/comment.ex
@@ -1,4 +1,4 @@
-defmodule Discuss.Topic do
+defmodule Discuss.Comment do
   use Ecto.Repo,
       otp_app: :discuss,
       adapter: Ecto.Adapters.Postgres
@@ -6,15 +6,17 @@ defmodule Discuss.Topic do
   import Ecto.Changeset
 
 
-  schema "topics" do
-    field :title, :string
+  schema "comments" do
+    field :content, :string
     belongs_to :user, Discuss.User
-    has_many :comments, Discuss.Comment
+    belongs_to :topic, Discuss.Topic
+
+    timestamps()
   end
 
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:title])
-    |> validate_required([:title])
+    |> cast(params, [:content])
+    |> validate_required([:content])
   end
 end
--- discuss/lib/discuss/user.ex
+++ discuss/lib/discuss/user.ex
@@ -11,6 +11,7 @@ defmodule Discuss.User do
     field :provider, :string
     field :token, :string
     has_many :topics, Discuss.Topic
+    has_many :comments, Discuss.Comment
 
     timestamps()
   end
