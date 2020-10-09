9fd5d99c8297b06ebe9f7b1b9e92d08e6d0b0c10
ymdarake <t.sakamoto.ouen@gmail.com>
2020-10-10 12:53:14 +1300

    [127]Publishing Messages

--- discuss/lib/discuss_web/channels/comments_channel.ex
+++ discuss/lib/discuss_web/channels/comments_channel.ex
@@ -3,13 +3,14 @@ defmodule DiscussWeb.CommentsChannel do
   use Phoenix.Channel
 
   def join(name, _params, socket) do
-		IO.puts("++++++++++")
-		IO.puts(name)
-		IO.puts("++++++++++")
 		{:ok, %{hey: "there"}, socket}
 	end
 
-	def handle_in() do
-
+	def handle_in(name, message, socket) do
+		IO.puts("+++++++")
+		IO.puts(name)
+		IO.inspect(message)
+		IO.puts("+++++++")
+		{:reply, :ok, socket}
 	end
 end
