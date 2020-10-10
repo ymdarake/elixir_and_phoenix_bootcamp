7e1bd648f18c5caeda9207d26785848b01c840fc
ymdarake <t.sakamoto.ouen@gmail.com>
2020-10-10 13:06:27 +1300

    [128 - 129]Refactoring to Determine Topic Id

--- discuss/assets/js/app.js
+++ discuss/assets/js/app.js
@@ -10,5 +10,5 @@ import "../css/app.scss"
 // Import deps with the dep name or local files with a relative path, for example:
 //
 //     import {Socket} from "phoenix"
-import socket from "./socket"
+import "./socket"
 import "phoenix_html"
--- discuss/lib/discuss_web/channels/comments_channel.ex
+++ discuss/lib/discuss_web/channels/comments_channel.ex
@@ -3,14 +3,11 @@ defmodule DiscussWeb.CommentsChannel do
   use Phoenix.Channel
 
   def join(name, _params, socket) do
+		IO.puts name
 		{:ok, %{hey: "there"}, socket}
 	end
 
 	def handle_in(name, message, socket) do
-		IO.puts("+++++++")
-		IO.puts(name)
-		IO.inspect(message)
-		IO.puts("+++++++")
 		{:reply, :ok, socket}
 	end
 end
