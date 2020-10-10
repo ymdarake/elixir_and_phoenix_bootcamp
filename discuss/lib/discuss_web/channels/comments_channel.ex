37b80120d29dfc679c6e27d8564cf37055624313
ymdarake <t.sakamoto.ouen@gmail.com>
2020-10-10 13:12:11 +1300

    [131]Debugging Topic Fetching

--- discuss/lib/discuss_web/channels/comments_channel.ex
+++ discuss/lib/discuss_web/channels/comments_channel.ex
@@ -1,10 +1,14 @@
 # https://hexdocs.pm/phoenix/channels.html#tying-it-all-together
 defmodule DiscussWeb.CommentsChannel do
   use Phoenix.Channel
+	alias Discuss.Repo
+	alias Discuss.Topic
 
-  def join(name, _params, socket) do
-		IO.puts name
-		{:ok, %{hey: "there"}, socket}
+  def join("comments:" <> topic_id, _params, socket) do
+		topic_id = String.to_integer(topic_id)
+		topic = Repo.get(Topic, topic_id)
+		IO.inspect topic
+		{:ok, %{}, socket}
 	end
 
 	def handle_in(name, message, socket) do
